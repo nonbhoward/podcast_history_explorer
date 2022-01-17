0438b1ce8c6672706e9aa1a8dcefa9828da5c01a
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2022-01-16 23:40:29 -0500

    init project structure

---
+++ main.py
@@ -0,0 +1,4 @@
+# the main program loop, the launcher
+# TODO
+#   1. read the database into a class
+#        
---
+++ tool/podcast.py
@@ -0,0 +1,6 @@
+from interface.zip import open_zip_file_located_
+
+
+class PodcastReader:
+    def __init__(self):
+        pass
