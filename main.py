816ed453f7d8998d941332f45306dc42638a6487
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2022-01-17 12:45:20 -0500

    better define open goals

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # the main program loop, the launcher
 # TODO
-#   1. read the database into a class
-#        
+#   1. locate the db object on disk
+#        how will location be configured?
+#   2. embed the db object into a reader class
+#   3. serve the reader class to a local browser
