e9c62711193aed90a17ca600168af84befa25eba
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2022-03-12 15:09:59 -0500

    init project sketch

---
+++ main.py
@@ -0,0 +1,19 @@
+# project sketch
+
+# local init
+#   define data path
+#   init available file metadata
+#   read available files into metadata
+
+# server launch
+#   define interface
+#     radio list selector
+#       unzip, delete,
+#       sub-file selector, auto identify type
+#         load, view,
+
+# database extras
+#   common-query-by-button
+
+# statistical extras
+#   (depends on available data)
