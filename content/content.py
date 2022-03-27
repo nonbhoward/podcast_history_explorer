a0ffaefa6e96035695f0ecc6027df69e6a833d41
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2022-03-27 17:33:59 -0400

    add page interconnect

--- content/content.py
+++ content/content.py
@@ -1,14 +1,18 @@
+local = '127.0.0.1:5000'
+
 root = \
-    """
+    f"""
     root
+    <a href="/branch">branch</a>
     """
 
 branch = \
-    """
+    f"""
     branch
+    <a href="/">root</a>
     """
 
 hello = \
-    """
+    f"""
     hello
     """
